0000220f44aad5f63f5d001eb013418434faea89
Energy0124 <energystar.0124@gmail.com>
2017-02-16 12:39:52 +0800

    Initial commit

---
+++ ConnectFour.py
@@ -0,0 +1,35 @@
+class ConnectFour:
+    # - Initialize the Connect Four game.
+    def __init__(self):
+        self.gameBoard = [[' '] * 6 for _ in range(7)]
+        # - This is a two-dimensional array representing the game board.
+        self.player1 = Human('O')
+        # - This is a variable representing Player O.
+        self.player2 = Computer('X')
+        # - This is a variable representing Player X.
+        self.turn = self.player1
+        # - This is a variable indicating the player that should play in the current turn.
+
+    # - Start a new game and play until winning/losing or draw.
+    def start_game(self):
+        pass
+
+    # - Print out the game board in the command line window.
+    def print_game_board(self):
+        pass
+
+
+class Player:
+    def __init__(self, player_symbol):
+        self.playerSymbol = player_symbol
+
+    def next_column(self, game_board):
+        pass
+
+
+class Human(Player):
+    pass
+
+
+class Computer(Player):
+    pass
