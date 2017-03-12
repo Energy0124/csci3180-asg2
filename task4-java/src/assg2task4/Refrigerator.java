b8c8c1896d2d199d3401e88205acddda88285683
Energy <energystar.0124@gmail.com>
2017-03-12 15:00:37 +0800

    add copyright

--- task4-java/src/assg2task4/Refrigerator.java
+++ task4-java/src/assg2task4/Refrigerator.java
@@ -1,3 +1,22 @@
+
+/*
+ *  CSCI3180 Principles of Programming Languages
+ *
+ *  --- Declaration ---
+ *
+ *  I declare that the assignment here submitted is original except for source
+ *  material explicitly acknowledged. I also acknowledge that I am aware of
+ *  University policy and regulations on honesty in academic work, and of the
+ *  disciplinary guidelines and procedures applicable to breaches of such policy
+ *  and regulations, as contained in the website
+ *  http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ *  Assignment 2
+ *  Name : Ling Leong
+ *  Student ID : 1155062557
+ *  Email Addr : alanalan0124@yahoo.com.hk
+ */
+
 package assg2task4;
 
 /*
